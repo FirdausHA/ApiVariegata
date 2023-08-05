89b0af573dbeb1ba83035080848a495a35509c9d
FirdausHA <firdausdemak279@gmail.com>
2023-08-05 08:24:29 +0700

    feat:[FH] Add product category

--- database/migrations/2023_08_05_004112_create_categories_table.php
+++ database/migrations/2023_08_05_004112_create_categories_table.php
@@ -13,12 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('obats', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image_url');
-            $table->decimal('price');
-            $table->string('description', 10000);
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('obats');
+        Schema::dropIfExists('categories');
     }
 };
