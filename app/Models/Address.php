a5db2faccd4cbe00d3c69f26c271cc31f63ef177
FirdausHA <firdausdemak279@gmail.com>
2023-08-29 09:45:53 +0700

    [FHA] Update Admin

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'address',
+        'receiver_name',
+        'receiver_phone',
+        'email',
+        'driver_note',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2023_08_21_224717_create_addresses_table.php
+++ database/migrations/2023_08_21_224717_create_addresses_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('address');
+            $table->string('receiver_name');
+            $table->string('receiver_phone');
+            $table->string('email');
+            $table->text('driver_note')->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
