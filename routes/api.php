06f68c2620ad3237a9807c4583db0686e96b1497
FirdausHA <firdausdemak279@gmail.com>
2023-09-14 00:32:30 +0700

    [FHA] Perbaikan Route Auth

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,10 @@ use App\Http\Controllers\OrderController;
 Route::get('/users', [AuthController::class, 'getAllUsers']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
