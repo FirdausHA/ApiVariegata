4f60d1ea0a2e2195d2050afabdde90ecd7999b63
FirdausHA <firdausdemak279@gmail.com>
2023-09-14 08:52:10 +0700

    [FHA] Update Product API

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'description', 'price', 'category_id', 'stock', 'image'];
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'image', 'stock'];
 
     public function category()
     {
