de8fdd53dc84578a364b26095a3cc081159ee19c
FirdausHA <firdausdemak279@gmail.com>
2023-09-17 20:51:00 +0700

    [FHA] Update API Product and Cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -93,22 +93,25 @@ class ProductController extends Controller
             return response()->json(['error' => 'Failed to update product'], 500);
         }
     }
-
     public function destroy($id)
     {
         try {
             $product = Product::findOrFail($id);
 
-            // Set the stock to zero before deleting the product
-            $product->stock = 0;
-            $product->save();
+            // Menghapus gambar produk dari penyimpanan
+            if ($product->image) {
+                Storage::disk('public')->delete($product->image);
+            }
 
+            // Menghapus produk dari database
             $product->delete();
-            return response()->json(['message' => 'Product deleted successfully'], 200);
+
+            return response()->json(['message' => 'Product deleted successfully']);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Failed to delete product'], 500);
         }
     }
+
 
 
     public function updateProductStock(Request $request, $productId)
